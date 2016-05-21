ab9093fdc2de797f98b31d322bd2944ef1a2ce06
Aorek <aquerorodriguez@gmail.com>
2016-05-21 18:32:55 +0200

    Add files
    First commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var iconImages = ["facebBW.png", "facebColor.png", "twitterBW.png", "twitterColor.png", "googleBW.png", "googleColor.png"]
+
+
+function iconoFaceb(number){
+	document.getElementById('iconFabe').src = "./images/" + iconImages[number];
+}
+
+function iconoTwitter(number){
+	document.getElementById('iconTwitter').src = "./images/" + iconImages[number];
+}
+
+function iconoGoogle(number){
+	document.getElementById('iconGoogle').src = "./images/" + iconImages[number];
+}
+
+
